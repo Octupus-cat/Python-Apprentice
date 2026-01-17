2376593bcf25c8b6dce0c6083275981041e31d52
Octupus-cat <Catherine@garibaldibros.com>
2026-01-17 20:58:41 +0000

    i started hotel manegment.

--- projects/98_WIndows/quiz_game.py
+++ projects/98_WIndows/quiz_game.py
@@ -42,7 +42,7 @@ questions = [  # ;
     "How many days in a non-leap year?",  # ;
 ]  # ;
 
-answers = ["python", "object orientated programming", "366"]  # ;
+answers = ["python", "object orientated programming", "365"]  # ;
 
 # Loop through each question in the questions list and ask the user for their response to each one using the simpledialog.askstring() method and an if statement to check if their response is correct or not and change the score accordingly using the score variable you created earlier.  # ;
 for i in range(len(questions)):  # ;
